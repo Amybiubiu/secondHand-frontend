eb1bb8deedaecb8d4622aff7be13f9d4685562e5
Amybiubiu <1246457100@qq.com>
2021-01-14 18:13:47 +0800

    add: message

--- src/pages/userAbout.jsx
+++ src/pages/userAbout.jsx
@@ -1,9 +1,8 @@
 import React,{useState, useEffect} from "react"
-import FetchData from '../fetch.js'
 import Fetch from '../service/fetch'
 import { makeStyles } from '@material-ui/core/styles'
 import Product from '../components/product'
-import pic1 from '../img/book1.JPG'
+import pic1 from '../img/default.png'
 import pic2 from '../img/book2.JPG'
 import pic3 from '../img/book3.JPG'
 
@@ -11,45 +10,48 @@ const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         width: '100%',
-        padding: '20px 80px'
+        padding: '20px 80px',
+        flexWrap: 'wrap'
     },
     item: {
-        margin: '20px'
+        margin: '20px',
+    },
+    img: {
+        width: '100px',
+        height: '100px',
+        borderRadius: '20px'
+    },
+    table: {
+        margin: '20px',
+        borderCollapse: 'collapse',
+        '& tr': {
+            borderBottom: `2px solid ${theme.palette.divider}`
+        }
+    },
+    commentDetail: {
+        width:230
     }
 }))
 export function UserPublish(){
     const classes = useStyles();
-    const obj1 = {
-        name:"smashing webkit",
-        category:"cs",
-        pic: pic1,
-        prices:"12.00r",
-        state:"1"
-    }
-    const obj2 = {
-        name:"社会学的邀请",
-        category: '社会科学',
-        pic: pic2,
-        state:"1"
-    }
-    const [state, setState] = useState([obj1,obj2]);
-    // useEffect(()=>{
-    //     Fetch('/user/publish?user_id=2018214877').then(
-    //         res => {
-    //             console.log(res.data)
-    //             setState(res.data)
-    //         }
-    //     )
-    // },[])
+    const [state, setState] = useState([]);
+    useEffect(()=>{
+        Fetch('/user/publish?user_id=2018214877').then(
+            res => {
+                console.log(res.data)
+                setState(res.data)
+            }
+        )
+    },[])
     return (
         <div className={classes.root}>
             {state.map(item =>(
                 <div className={classes.item}>
-                    <Product name={item.name} 
-                    category={item.category} 
-                    pic={item.pic} 
-                    prices={item.prices} 
-                    state={item.state} />
+                    <Product name={item.product_name} 
+                    category={item.product_class} 
+                    pic={item.product_pic} 
+                    prices={item.product_price} 
+                    state={item.product_state} />
                 </div>
             ))}
         </div>
@@ -57,13 +59,125 @@ export function UserPublish(){
 }
 
 export function UserBuy(){
-    return(
-        <div>my buy</div>
+    const classes = useStyles();
+    const [state, setState] = useState([]);
+    useEffect(()=>{
+        Fetch('/user/buy?user_id=2018214877').then(
+            res => {
+                console.log(res.data)
+                setState(res.data)
+            }
+        )
+    },[])
+    return (
+        <div className={classes.root}>
+            {state.map(item =>(
+                <div className={classes.item} key={item.product_id}>
+                    <Product name={item.product_name} 
+                    category={item.product_class} 
+                    pic={item.product_pic} 
+                    prices={item.product_price} 
+                    state={item.product_state} />
+                </div>
+            ))}
+        </div>
     )
 }
 
 export function UserSell(){
+    const classes = useStyles();
+    const [state, setState] = useState([]);
+    useEffect(()=>{
+        Fetch('/user/sell?user_id=2018214877').then(
+            res => {
+                console.log(res.data)
+                setState(res.data)
+            }
+        )
+    },[])
+    return (
+        <div className={classes.root}>
+            {state.map(item =>(
+                <div className={classes.item} key={item.product_id}>
+                    <Product name={item.product_name} 
+                    category={item.product_class} 
+                    pic={item.product_pic} 
+                    prices={item.product_price} 
+                    state={item.product_state} />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export function ReceiveMessage(){
+    const [state, setState] = useState([])
+    const classes = useStyles();
+    useEffect(()=>{
+        Fetch('/comments/receive?user_id=2018214877').then(
+            res => {
+                setState(res.data);
+            }
+        )
+    }, [])
+    return (
+        <div>
+            <table className={classes.table}>
+                <tr style={{fontWeight: '500'}}>
+                    <td>评论内容</td>
+                    <td style={{minWidth: '300px'}}>{`商品信息`}</td>
+                    <td>评论人</td>
+                </tr>
+                {state.map((item, index) =>
+                    <tr>
+                        <td className={classes.commentDetail}>{item.comment_detail}</td>
+                        <td>
+                            <img className={classes.img} src={item.product_pic||pic1} alt="书本封面"/>
+                            <div style={{display:'inline-block', verticalAlign:'top'}}>
+                                <div style={{margin: '18px 0'}}>{item.product_name}</div>
+                                <div style={{margin: '18px 0'}}>{item.product_describe}</div>
+                            </div>
+                        </td>
+                        <td>{item.user_name}</td>
+                    </tr>
+                )}
+            </table>
+        </div>
+    )
+}
+
+export function CommentMessage(){
+    const [state, setState] = useState([])
+    const classes = useStyles();
+    useEffect(()=>{
+        Fetch('/comments/send?user_id=2018214877').then(
+            res => {
+                setState(res.data)
+            }
+        )
+    },[])
     return(
-        <div>my sell</div>
+        <div>
+           <table className={classes.table}>
+                <tr style={{fontWeight: '500'}}>
+                    <td>评论内容</td>
+                    <td style={{minWidth: '300px'}}>{`商品信息`}</td>
+                    <td>商品所有人</td>
+                </tr>
+                {state.map((item, index) =>
+                    <tr>
+                        <td className={classes.commentDetail}>{item.comment_detail}</td>
+                        <td>
+                            <img className={classes.img} src={item.product_pic||pic1} alt="书本封面"/>
+                            <div style={{display:'inline-block', verticalAlign:'top'}}>
+                                <div style={{margin: '18px 0'}}>{item.product_name}</div>
+                                <div style={{margin: '18px 0'}}>{item.product_describe}</div>
+                            </div>
+                        </td>
+                        <td>{item.user_name}</td>
+                    </tr>
+                )}
+            </table>
+        </div>
     )
 }
